764ade3d789857f1b72daed58418e2532570a7dd
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-11-16 15:06:46 +0900

    change response format

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    json!({"message": "Hello, world!"})
 }
 
 #[get("/gb")]
 fn gb() -> &'static str {
-    "Goodbye!"
-}#[get("/ga")]
-fn ga() -> &'static str {
-    "Good Afternoon!"
+    json!({"message": "Goodbye!"})
 }
 
-fn main(){
+#[get("/ga")]
+fn ga() -> &'static str {
+    json!({"message": "Good Afternoon!"})
+}
+
+fn main() {
     rocket::ignite()
         .mount("/", routes![index, ga, gb])
         .launch();
